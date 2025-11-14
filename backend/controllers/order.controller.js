982504b15d30b7e3ffa0b8689cf4704e5abbf249
joaovcasas <jo193545@ucf.edu>
2025-11-14 15:39:23 -0500

    Update order.controller.js
    Update the oder controller to include update of final checkout

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,6 +1,7 @@
 import Order from "../models/order.model.js";
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
+import Food from "../models/food.model.js";
 
 export const getOrder = async(req, res) =>{
         // Send ID, so we can grab it!
@@ -10,30 +11,66 @@ export const getOrder = async(req, res) =>{
         if(!mongoose.Types.ObjectId.isValid(id)){
             return res.status(404).json({success: "false", message: "Invalid user ID!"});
         }
-
-        // input will be the user's cart
-        const order = req.body;
         
-        const newOrder = new Order(order);
-        // Create new Order
         const tempUser = await User.findById(id);
 
-        // EDIT THIS
-        console.log(tempUser);
+        // Create a new Order
+        const newOrder = new Order({
+         userId : tempUser._id,
+         status : "Preparing",
+         items : tempUser.cart
+       });
 
-        newOrder.userId = tempUser._id;
-        newOrder.status = "Preparing";
-        newOrder.items = tempUser.cart;
-
-        console.log(newOrder);
+        // Get totals
+        let subtotal = 0;
+        let tax = 1.07; 
+        let total = 0;
+        let orderLength = newOrder.items.length;
         
-        // Check for it in database!
+       
+       // Loop through cart
+
         try {
-            await User.findById(id);
+            for (let i = 0; i < orderLength; i++) {
+            const item = newOrder.items[i];
+            const foodID = item.foodId;
+            const quantity = item.qty;
             
-            res.status(200).json({success: true, message: " Order has been saved!"});
+
+            const food = await Food.findById(foodID);
+
+            if (!food) {
+                console.log(`Food not found for ID: ${foodID}`);
+                continue;
+            }
+
+            // get price and calculate subtotal
+            const price = food.price;
+            subtotal += price * quantity;
+
+        }
+        // Update total and subtotals
+        total = subtotal * tax;
+        total = parseFloat(total.toFixed(2));
+        subtotal = parseFloat(subtotal.toFixed(2));
+
+        // Add to the order
+        newOrder.subtotal = subtotal
+        newOrder.tax = tax;
+        newOrder.total = total 
+
+        // UPDATE POINTS EARNED
+        const points_earned = Math.round(total * 0.1);
+        const total_points = + tempUser.points + points_earned;
+        
+        await User.updateOne({_id: id}, {points: total_points});
+
+        res.status(200).json({success: true, message: "Order has been saved!", order: newOrder, user: tempUser});
         } catch (error) {
             console.log("error in getting order:", error.message);
             res.sendStatus(500).json({sucess: false, message: "Server Error"});
         }
+
+
 }
+
