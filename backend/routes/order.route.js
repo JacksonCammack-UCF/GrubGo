a8064920a545e6aa9413943183b165aeddbeebc8
willjucf <wi698816@ucf.edu>
2025-11-16 02:56:58 -0500

    Will add Order history to order controller and a small bug fix to get order.

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getOrder } from "../controllers/order.controller.js";
+import { getOrder, getUserOrderHistory } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
-// ORDER ROUTES
-router.get("/:id", getOrder);
+// ORDER ROUTES // will changed to post
+router.post("/:id", getOrder);
 
+// will order history
+router.get("/history/:userId", getUserOrderHistory);
 
 export default router;
